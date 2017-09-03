8d5248d3c28a843764ac1a26aa20b2e864dfda27
Jonno <jonnowitts@gmail.com>
2017-09-03 13:05:48 +0100

    Added animation to homepage elements

--- script/script.js
+++ script/script.js
@@ -1,8 +1,38 @@
+const   viewPortSize = $(window).height(),
+        scrollToLast = $('#scrollToLast'),
+        scrollToFeatured = $('#scrollToFeatured'),
+        bounceClasses = 'bouncy fadeIn',
+        triggerAt = scrollToLast.offset().top,
+        triggerHeight = triggerAt - viewPortSize;
+var     lastBounce = false;
+
 $(function(){
-    $('#scrollToFeatured').on('click', function(e){
+    scrollToFeatured.on('click', function(e){
         e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('#_featured').offset().top
-        }, 800);
+        scrollTo($(this).attr('href'));
     });
+
+    scrollToLast.on('click', function(e){
+        e.preventDefault();
+        scrollTo($(this).attr('href'));
+    });
+});
+
+function scrollTo(href) {
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, 800);
+}
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= triggerHeight) {
+       scrollToFeatured.removeClass(bounceClasses);
+       scrollToLast.addClass(bounceClasses);
+       lastBounce = true;
+    }
+    else if(lastBounce) {
+        scrollToFeatured.addClass(bounceClasses);
+        scrollToLast.removeClass(bounceClasses);
+        lastBounce = false;
+    }
 });
