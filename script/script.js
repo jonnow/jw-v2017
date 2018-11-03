e98829e4c0af6e5fbe82dcf311357f551f718ea1
Jonno <jonnowitts@gmail.com>
2018-11-03 17:19:32 +0000

    Folder housekeeping
    Moved the "Live" folder to be the main file. Archived and zipped the rest.

--- script/script.js
+++ script/script.js
@@ -1,26 +1,35 @@
-const   viewPortSize = $(window).height(),
-        scrollToLast = $('#scrollToLast'),
-        scrollToFeatured = $('#scrollToFeatured'),
-        bounceClasses = 'bouncy fadeIn',
-        triggerAt = scrollToLast.offset().top,
-        triggerHeight = triggerAt - viewPortSize;
-var     lastBounce = false;
+const   bounceClasses = 'bouncy fadeIn',
+        viewPortSize = $(window).height();
+
+var     scrollToLast,
+        scrollToFeatured,
+        triggerAt,
+        triggerHeight,
+        lastBounce = false;
 
 $(function(){
-    scrollToFeatured.on('click', function(e){
-        e.preventDefault();
-        scrollTo($(this).attr('href'));
-    });
+    if($('body').hasClass('home')) {
+        scrollToLast = $('#scrollToLast');
+        scrollToFeatured = $('#scrollToFeatured');
+        triggerAt = scrollToLast.offset().top;
+        triggerHeight = triggerAt - viewPortSize;
+    
+        scrollToFeatured.on('click', function(e){
+            e.preventDefault();
+            scrollTo($(this).attr('href'));
+        });
 
-    scrollToLast.on('click', function(e){
-        e.preventDefault();
-        scrollTo($(this).attr('href'));
-    });
+        scrollToLast.on('click', function(e){
+            e.preventDefault();
+            scrollTo($(this).attr('href'));
+        });
 
-    // prevents the animation from playing on screen resize
-    setTimeout(function(){
-        $('body').hasClass('home') ? $('body').addClass('ac') : void 0; // Animation complete
-    }, 3250);
+        // prevents the animation from playing on screen resize
+        setTimeout(function(){
+            $('body').hasClass('home') ? $('body').addClass('ac') : void 0; // Animation complete
+        }, 3250);
+
+    }
 });
 
 function scrollTo(href) {
